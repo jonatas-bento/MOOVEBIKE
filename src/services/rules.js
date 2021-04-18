f15a1db4b1bfff2f6ba3db0e620665ffd89fcdcb
ANNEBORTOLI <annebortoli@gmail.com>
2021-04-18 19:13:23 -0300

    Padronização CSS fase 1

--- src/services/rules.js
+++ src/services/rules.js
@@ -1,28 +1,28 @@
 const rules = [
     {
-        photo: "/img/two-removebg-preview.png",
+        photo: "/img/one-removebg-preview.png",
         title: "CADASTRO",
-        info: "Cadastre-se em nosso site.",   
+        info: "Cadastre-se em nosso site.",
     },
     {
         photo: "/img/two-removebg-preview.png",
         title: "PLANO",
-        info: "Escolha um plano. Temos planos: diário, semanal, quinzenal e mensal.",    
+        info: "Escolha um plano. Temos planos: diário, semanal, quinzenal e mensal.",
     },
     {
         photo: "/img/threepg-removebg-preview.png",
         title: "RESERVA",
-        info: "Faça sua reserva online.",    
+        info: "Faça sua reserva online.",
     },
     {
         photo: "/img/four-removebg-preview.png",
         title: "RETIRADA/DEVOLUÇÃO",
-        info: "Escolha a loja mais próxima de você para retirar e devolver.",    
+        info: "Escolha a loja mais próxima de você para retirar e devolver.",
     }
 ]
 
 const getRules = () => {
     return rules
 }
-  
+
 module.exports = { getRules }
